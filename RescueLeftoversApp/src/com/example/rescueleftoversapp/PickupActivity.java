817260212d859a1ca370d13e856cfee255ebebcf
Stanley <stanley.yang.sy@gmail.com>
2014-10-25 07:30:07 -0700

    stashing

--- RescueLeftoversApp/src/com/example/rescueleftoversapp/PickupActivity.java
+++ RescueLeftoversApp/src/com/example/rescueleftoversapp/PickupActivity.java
@@ -1,24 +1,49 @@
 package com.example.rescueleftoversapp;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v4.app.Fragment;
+import java.util.HashMap;
+import java.util.Random;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBarActivity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class PickupActivity extends ActionBarActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_pickup);
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment()).commit();
-        }
+        HashMap<String, String> addresses = new HashMap();
+        addresses.put("Bobby Van\'s Steakhouse", "230 Park Avenue, New York, NY 10169");
+        addresses.put("Jiannetto\'s Pizza and Catering", "47 St Park Ave, Manhattan, NY 10017");
+        addresses.put("Chipotle Mexican Grill", "320 Park Avenue, New York, NY 10022");
+        addresses.put("Cosi", "38 E 45th Street, New York, NY 10017");
+        addresses.put("Pret A Manger", "425 Madison, New York, NY 10017");
+        // Get the message from the intent
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MapActivity.EXTRA_MESSAGE);
+
+        String endTime = message.substring(message.length() - 7, message.length());
+        String startTime = message.substring(message.length() - 15, message.length() - 8);
+        String date = message.substring(message.length() - 32, message.length() - 16);
+        String name = message.substring(0, message.length() - 32);
+        setTitle(name);
+        // Create the text view
+        TextView textView = new TextView(this);
+        textView.setTextSize(20);
+        Random rand = new Random();
+        String s = String.format("Name: %s %nAddress: %s %nDate: %s %nStart Time: %s %nEnd Time: %s %nTotal Weight: %d %nAvailable slots: %d", 
+                                 name + intent.getStringExtra(MapActivity.CURRENT_MARKER), addresses.get(name), date, startTime, endTime, (rand.nextInt(6) + 5) * 10, name.length() % 3);
+        textView.setText(s);
+
+        // Set the text view as the activity layout
+        setContentView(textView);
     }
 
     @Override
