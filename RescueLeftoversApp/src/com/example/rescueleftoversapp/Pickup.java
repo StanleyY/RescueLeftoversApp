44aa2f941bf06fc60979f116e28c066ab7397cf4
sh972 <sh972@cornell.edu>
2014-10-25 05:12:10 -0400

    pickup simplified

--- RescueLeftoversApp/src/com/example/rescueleftoversapp/Pickup.java
+++ RescueLeftoversApp/src/com/example/rescueleftoversapp/Pickup.java
@@ -3,21 +3,21 @@ package com.example.rescueleftoversapp;
 import java.util.ArrayList;
 
 public class Pickup {
-	private String address;
-	private String restaurantName;
-	private Date date;
-	private Time StartTime;
-	private Time EndTime;
-	private int totalWeight;
-	private int accumulatedWeight;
-	private String description;
-	private int numVolunteers;
-	private ArrayList<UserWeightTuple> attendants;
-	private ArrayList<UserWeightTuple> waitlist;
-	private boolean visibleToVolunteers; 
-	private LeaderUser leader;
+	public String address;
+	public String restaurantName;
+	public String date;
+	public String StartTime;
+	public String EndTime;
+	public int totalWeight;
+	public int accumulatedWeight;
+	public String description;
+	public int numVolunteers;
+	public ArrayList<UserWeightTuple> attendants;
+	public ArrayList<UserWeightTuple> waitlist;
+	public boolean visibleToVolunteers; 
+	public LeaderUser leader;
 	
-	public Pickup (String restaurant, Date d, Time start, Time end, String addr, int weight, String desc) {
+	public Pickup (String restaurant, String d, String start, String end, String addr, int weight, String desc) {
 		address = addr;
 		restaurantName = restaurant;
 		date = d;
@@ -33,7 +33,7 @@ public class Pickup {
 		leader = null;
 	}
 	
-	public Pickup (String restaurant, Date d, Time start, Time end, String addr, int weight) {
+	public Pickup (String restaurant, String d, String start, String end, String addr, int weight) {
 		address = addr;
 		restaurantName = restaurant;
 		date = d;
@@ -47,69 +47,6 @@ public class Pickup {
 		waitlist = new ArrayList<UserWeightTuple>();
 		visibleToVolunteers = false;
 		leader = null;
-	}
-	
-	public String getRestaurantName () {
-		return restaurantName;
-	}
-	public void setRestaurantName (String name) {
-		restaurantName = name;
-	}
-	
-	public String getAddress() {
-		return address;
-	}
-	public void setAddress(String add) {
-		address = add;
-	}
-	
-	public Date getDate () {
-		return date;
-	}
-	public void setDate (Date d) {
-		date = d;
-	}
-	
-	public Time getStart () {
-		return StartTime;
-	}
-	public void setStart (Time time) {
-		StartTime = time;
-	}
-	
-	public Time getEnd () {
-		return EndTime;
-	}
-	public void setEnd (Time time) {
-		EndTime = time;
-	}
-	
-	public int getTotal () {
-		return totalWeight;
-	}
-	public void setTotal (int t) {
-		totalWeight = t;
-	}
-	
-	public int getAccumulated () {
-		return accumulatedWeight;
-	}
-	public void setAccumulated (int weight) {
-		accumulatedWeight = weight;
-	}
-	
-	public String getDescription () {
-		return description;
-	}
-	public void setDescription (String desc) {
-		description = desc;
-	}
-	
-	public int getNumVolunteers () {
-		return numVolunteers;
-	}
-	public void setNumVolunteers (int n) {
-		numVolunteers = n;
 	}
 	
 	public ArrayList<UserWeightTuple> getAttendants () {
@@ -179,33 +116,6 @@ public class Pickup {
 			return true;
 		} else {
 			return false;
-		}
-	}
-	
-	public boolean overlaps (Pickup p) {
-		Date d = p.getDate();
-		if ((date.getMonth() == d.getMonth()) && (date.getDay() == d.getDay()) && (date.getYear() == d.getYear())) {
-			Time s = p.getStart();
-			Time e = p.getEnd();
-			if ((StartTime.compareTo(s) >= 0) && (EndTime.compareTo(s) <= 0)) {
-				return true;
-			} else if ((StartTime.compareTo(e) >= 0) && (EndTime.compareTo(e) <= 0)) {
-				return true;
-			}
-		}
-		return false;
-	}
-	
-	public boolean earlierThan (Pickup p) {
-		Date d = p.getDate();
-		if (date.getYear() != d.getYear()) {
-			return date.getYear() < d.getYear();
-		} else if (date.getMonth() != d.getMonth()) {
-			return date.getMonth() < d.getMonth();
-		} else if (date.getDay() != d.getDay()) {
-			return date.getDay() < d.getDay();
-		} else {
-			return (StartTime.compareTo(p.getStart()) > 0);
 		}
 	}
 	
